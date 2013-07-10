fff3a386240637cd2dd60e52795142b2b1595fbb
Noe Aguilar <naguilar_99@yahoo.com>
2013-07-10 04:21:11 -0500

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,6 @@ buffer = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
   response.send(buffer.toString('utf-8'));
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
