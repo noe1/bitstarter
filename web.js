f00520b9a4cc184162a0873ebc74c170eacf202d
Noe Aguilar <naguilar_99@yahoo.com>
2013-07-10 04:29:11 -0500

    Updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 app.get('/', function(request, response) {
   response.send('Hello World 5!');
 });
