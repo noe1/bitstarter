974b19eb0b281f81228dac4ca7531c57f69f4163
Noe Aguilar <naguilar_99@yahoo.com>
2013-07-10 04:32:16 -0500

    Updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
+var buffer = new Buffer(16);
+buffer = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
   response.send('Hello World 5!');
 });
