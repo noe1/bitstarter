1197b72e5494fc96a02ac282026187b1fc340b85
Noe Aguilar <naguilar_99@yahoo.com>
2013-07-10 04:15:06 -0500

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
+var buffer = new Buffer(16);
 var app = express.createServer(express.logger());
-
+buffer = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
