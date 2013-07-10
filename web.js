09e8fe8c0fa6bcf1de910ba17e65f56cc140d635
Noe Aguilar <naguilar_99@yahoo.com>
2013-07-10 04:23:17 -0500

    Updated web.js

--- web.js
+++ web.js
@@ -4,8 +4,9 @@ var buffer = new Buffer(16);
 var app = express.createServer(express.logger());
 buffer = fs.readFileSync("index.html");
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send('Hello World 3!');
 });
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
